f9fd13a0eed646183d921baa61d9ff7ca9f59992
Sebastian Miozga <miozga.sebastian@gmail.com>
2025-01-27 11:36:59 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,29 @@
+document.body.style.height = 10000 + "vh";
+
+const squere = document.createElement("div"); //tworzymy div
+document.body.appendChild(squere);
+
+let grow = true; // tzw flaga, zawsze jest true albo false
+let size = 50; //deklarujemy początkową wielkość kwadratu
+
+squere.style.height = size + "px";
+//zamiast danych kopiujemy zmienną, konkatenacja i tworzymy string z px  co może
+window.addEventListener("scroll", function () {
+  if (size >= window.innerHeight / 2) {
+    grow = !grow;
+    squere.style.backgroundColor = "red";
+  } else if (size <= 0) {
+    grow = !grow;
+    squere.style.backgroundColor = "green";
+  }
+
+  if (grow) {
+    size += 5;
+
+    squere.style.height = size + "px";
+  } else {
+    size -= 5;
+
+    squere.style.height = size + "px";
+  }
+});
